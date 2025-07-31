f95a0a7363b0638b67d6ca03f7a8f2d8e50d6baa
sumedhav0501 <sumedhav@aurelza.com>
2025-07-31 19:40:42 +0530

    redis partially removed for deployment testing

--- backend/services/searchService.js
+++ backend/services/searchService.js
@@ -1,12 +1,12 @@
-const redis = require('../utils/redisClient');
+// const redis = require('../utils/redisClient');
 const { index } = require('../utils/meilisearchClient');
 
 async function searchVehicles(query,carLimit=500) {
 
-  console.log(process.env.MEILI_MASTER_KEY);
-  const cacheKey = `search:${query.toLowerCase()}`;
-  const cached = await redis.get(cacheKey);
-  if (cached) return JSON.parse(cached);
+  // console.log(process.env.MEILI_MASTER_KEY);
+  // const cacheKey = `search:${query.toLowerCase()}`;
+  // const cached = await redis.get(cacheKey);
+  // if (cached) return JSON.parse(cached);
 
 
   const results = await index.search(query, {
@@ -15,7 +15,7 @@ async function searchVehicles(query,carLimit=500) {
     attributesToHighlight: ['fullName'],
   });
 
-  await redis.set(cacheKey, JSON.stringify(results), 'EX', 3600);
+  // await redis.set(cacheKey, JSON.stringify(results), 'EX', 3600);
   return results;
 }
 
--- backend/utils/redisClient.js
+++ backend/utils/redisClient.js
@@ -1,6 +1,6 @@
-const Redis = require('ioredis');
-const redis = new Redis({
-  host: process.env.REDIS_HOST || 'localhost',
-  port: 6379,
-});
-module.exports = redis;
+// const Redis = require('ioredis');
+// const redis = new Redis({
+//   host: process.env.REDIS_HOST || 'localhost',
+//   port: 6379,
+// });
+// module.exports = redis;
